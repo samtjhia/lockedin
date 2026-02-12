9787295f2d4240fd6563a1534ce5dfc3c74255e8
Samuel Tjhia <110407645+samtjhia@users.noreply.github.com>
2026-02-12 13:04:35 -0500

    add google sign in

---
+++ src/app/login/actions.ts
@@ -0,0 +1,66 @@
+'use server'
+
+import { revalidatePath } from 'next/cache'
+import { redirect } from 'next/navigation'
+import { headers } from 'next/headers'
+
+import { createClient } from '@/utils/supabase/server'
+
+export async function login(formData: FormData) {
+  const supabase = await createClient()
+
+  const email = formData.get('email') as string
+  const password = formData.get('password') as string
+
+  const { error } = await supabase.auth.signInWithPassword({
+    email,
+    password,
+  })
+
+  if (error) {
+    return redirect('/login?error=Could not authenticate user')
+  }
+
+  revalidatePath('/', 'layout')
+  redirect('/')
+}
+
+export async function signup(formData: FormData) {
+  const supabase = await createClient()
+
+  const email = formData.get('email') as string
+  const password = formData.get('password') as string
+  
+  const { error } = await supabase.auth.signUp({
+    email,
+    password,
+  })
+
+  if (error) {
+    return redirect('/login?error=Could not create user')
+  }
+
+  revalidatePath('/', 'layout')
+  redirect('/')
+}
+
+export async function signInWithGoogle() {
+  const supabase = await createClient()
+  const origin = (await headers()).get('origin')
+
+  const { data, error } = await supabase.auth.signInWithOAuth({
+    provider: 'google',
+    options: {
+      redirectTo: `${origin}/auth/callback`,
+    },
+  })
+
+  if (error) {
+    redirect('/login?error=Could not authenticate user')
+  }
+
+  if (data.url) {
+    redirect(data.url)
+  }
+}
+
