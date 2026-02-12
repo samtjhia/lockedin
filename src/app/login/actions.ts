f5dd7a3d4f3bece4fc0e2d8d1366210149f29eda
Samuel Tjhia <110407645+samtjhia@users.noreply.github.com>
2026-02-12 13:42:54 -0500

    fix login redirection

--- src/app/login/actions.ts
+++ src/app/login/actions.ts
@@ -30,7 +30,7 @@ export async function login(formData: FormData) {
   }
 
   revalidatePath('/', 'layout')
-  redirect('/')
+  redirect('/dashboard')
 }
 
 export async function signup(formData: FormData) {
